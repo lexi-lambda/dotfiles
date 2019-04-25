bdfe87c61d0685fb11291fc9b4361de10f8fba66
Alexis King <lexi.lambda@gmail.com>
2019-04-25 13:34:30 -0500

    bin/support: Improve ~id and ~id/1

--- bin/support/syntax.rkt
+++ bin/support/syntax.rkt
@@ -9,35 +9,42 @@
          "contract.rkt")
 
 (provide (contract-out
-          [~id (->i () (#:context [context (or/c syntax? #f)]
-                        #:source [source (props) (if (unsupplied-arg? props)
-                                                     (or/c syntax? #f)
-                                                     source-location?)]
-                        #:props [props (or/c syntax? #f)]
-                        #:sub-range-binders [sub-range-binder-introduce
-                                             (or/c (-> syntax? syntax?) #f)]
-                        #:binder? [binder? any/c])
+          [~id (->* [] [#:context (or/c syntax? #f)
+                        #:source source-location?
+                        #:props (or/c syntax? #f 'infer)
+                        #:sub-range-binders (or/c #t (-> syntax? syntax?) #f)
+                        #:binder? any/c]
                     #:rest (listof (or/c identifier? string? symbol? keyword? char? number?))
-                    [_ identifier?])]
+                    identifier?)]
           [~id/1 (->* [] [#:context (or/c syntax? #f 'infer)
                           #:source (or/c source-location? 'infer)
                           #:props (or/c syntax? #f 'infer)
-                          #:sub-range-binders (or/c (-> syntax? syntax?) #f)
+                          #:sub-range-binders (or/c #t (-> syntax? syntax?) #f)
                           #:binder? any/c]
                       #:rest (and/c (listof (or/c identifier? string? symbol? keyword? char? number?))
                                     list-contains-exactly-one-identifier?)
                       identifier?)]))
 
+(define (jumble->list v p?)
+  (let recur ([v v])
+    (cond
+      [(p? v) (list v)]
+      [(list? v) (append-map recur v)]
+      [(pair? v) (append (recur (car v)) (recur (cdr v)))]
+      [else '()])))
+
 (define (~id #:context [context #f]
              #:source [source context]
-             #:props [props source]
-             #:sub-range-binders [sub-range-binder-introduce
-                                  (if (syntax-transforming?)
-                                      syntax-local-introduce
-                                      values)]
+             #:props [props 'infer]
+             #:sub-range-binders [sub-range-binders #t]
              #:binder? [binder? #t]
              . args)
-
+  (define sub-range-binder-introduce (and sub-range-binders
+                                          (if (procedure? sub-range-binders)
+                                              sub-range-binders
+                                              (if (syntax-transforming?)
+                                                  syntax-local-introduce
+                                                  values))))
   (define-values [pieces ids]
     (for/fold ([pieces '()]
                [pos 0]
@@ -49,7 +56,7 @@
         (values
          (cons piece pieces)
          (+ pos piece-length)
-         (if (and sub-range-binder-introduce id)
+         (if (and sub-range-binders id)
              (cons (vector-immutable (sub-range-binder-introduce id) piece-length pos) ids)
              ids)))
       (match arg
@@ -63,17 +70,59 @@
   (define new-id (datum->syntax context
                                 (string->symbol (string-append* (reverse pieces)))
                                 (build-source-location-vector source)
-                                props))
+                                ; If we’re attaching 'sub-range-binders, then we don’t want to copy
+                                ; properties, since 'sub-range-binders doesn’t care about
+                                ; originalness, and in fact it will probably do more harm than good.
+                                ; But if context is provided and we’re not attaching
+                                ; 'sub-range-binders, then we probably want to copy originalness,
+                                ; after all.
+                                (if (eq? props 'infer)
+                                    (if sub-range-binders #f context)
+                                    props)))
 
   (cond
     [(not (empty? ids))
+     ; Build a 'sub-range-binders for each given identifier.
      (define (build-prop-value id)
+       (define new-id-introduced (sub-range-binder-introduce new-id))
        (match-define (vector old-id old-id-len new-id-start) id)
-       (if binder?
-           (vector-immutable new-id new-id-start old-id-len 0.5 0.5
-                             old-id 0 old-id-len 0.5 0.5)
-           (vector-immutable old-id 0 old-id-len 0.5 0.5
-                             new-id new-id-start old-id-len 0.5 0.5)))
+
+       ; If the identifier already has a 'sub-range-binders property on it, then in all likelihood
+       ; that means it was just created in the dynamic extent of the same macro transformer that
+       ; added it in the first place. This means that the identifier may be being built in several
+       ; steps, using multiple calls to ~id in sequence. Therefore, we want to copy over old
+       ; 'sub-range-binders values that are already present if one side of the binding arrow points
+       ; to the old identifier.
+       ;
+       ; Conservatively, we use bound-identifier=? to check if the identifier is, in fact, the
+       ; “same”, since if we’re really in the same macro transformer, it’s unlikely other scopes
+       ; have been added in between, anyway.
+       (define (relocate-old-prop-value val)
+         (match-define (vector binder-id binder-id-start binder-id-range binder-id-x binder-id-y
+                               use-id use-id-start use-id-range use-id-x use-id-y)
+           val)
+         (cond
+           [(bound-identifier=? binder-id old-id)
+            (vector-immutable new-id-introduced (+ binder-id-start new-id-start) binder-id-range
+                              binder-id-x binder-id-y
+                              use-id use-id-start use-id-range use-id-x use-id-y)]
+           [(bound-identifier=? use-id old-id)
+            (vector-immutable binder-id binder-id-start binder-id-range binder-id-x binder-id-y
+                              new-id-introduced (+ use-id-start new-id-start) use-id-range
+                              use-id-x use-id-y)]
+           [else
+            #f]))
+       (define old-values (jumble->list (syntax-property old-id 'sub-range-binders) vector?))
+       (define relocated-old-values (filter-map relocate-old-prop-value old-values))
+
+       (define new-value
+         (if binder?
+             (vector-immutable new-id-introduced new-id-start old-id-len 0.5 0.5
+                               old-id 0 old-id-len 0.5 0.5)
+             (vector-immutable old-id 0 old-id-len 0.5 0.5
+                               new-id-introduced new-id-start old-id-len 0.5 0.5)))
+       (if (empty? relocated-old-values) new-value (cons new-value relocated-old-values)))
+
      (syntax-property new-id 'sub-range-binders (map build-prop-value ids))]
     [else
      new-id]))
@@ -82,12 +131,9 @@
   (= (count identifier? lst) 1))
 
 (define (~id/1 #:context [context 'infer]
+               #:source [source 'infer]
                #:props [props 'infer]
-               #:sub-range-binders [sub-range-binder-introduce
-                                    (if (syntax-transforming?)
-                                        syntax-local-introduce
-                                        values)]
-               #:source [source (if sub-range-binder-introduce #f 'infer)]
+               #:sub-range-binders [sub-range-binder-introduce #t]
                #:binder? [binder? #t]
                . args)
   (define the-id (first (filter identifier? args)))
@@ -95,6 +141,6 @@
   (apply ~id args
          #:context (infer-or context)
          #:source (infer-or source)
-         #:props (infer-or props)
+         #:props props
          #:sub-range-binders sub-range-binder-introduce
          #:binder? binder?))
