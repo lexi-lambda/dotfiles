646bca2244f8c3649af86816d4bf7d821d79db24
Alexis King <lexi.lambda@gmail.com>
2019-06-30 10:51:53 -0500

    fish: Rewrite prompt implementation
    I wrote the old one years ago, when I knew basically nothing about
    either fish or shell programming in general. This basically just tidies
    things up.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,72 +1,61 @@
 function fish_prompt
-  set -l red       (tput setaf  1)
-  set -l green     (tput setaf  2)
-  set -l yellow    (tput setaf  3)
-  set -l blue      (tput setaf  4)
-  set -l magenta   (tput setaf  5)
-  set -l cyan      (tput setaf  6)
-  set -l lgray     (tput setaf  7)
-  set -l gray      (tput setaf  8)
-  set -l lred      (tput setaf  9)
-  set -l lgreen    (tput setaf 10)
-  set -l lyellow   (tput setaf 11)
-  set -l lblue     (tput setaf 12)
-  set -l lmagenta  (tput setaf 13)
-  set -l lcyan     (tput setaf 14)
-  set -l white     (tput setaf 15)
-  set -l black     (tput setaf 16)
-  set -l bold      (tput bold)
-  set -l underline (tput smul)
-  set -l reset     (tput sgr0)
-
-  set -l location (
-    if [ (pwd) = "$HOME" ]
-        echo "$blue～"
-    else
-        set -l parent_dir (dirname (pwd))
-        if [ "$parent_dir" != "$HOME" ]
-          echo -ns "$blue" (basename $parent_dir) " → "
-        end
-        echo -s "$green" (basename (pwd))
-    end
-  )
-
+  set -l command_status $status
   set -l question_mark_in_circle '?'\u20dd
+  set_color red; echo -n '# '
+
+  # print current location
+  if [ "$PWD" = "$HOME" ]
+    set_color green; echo -n '~'
+  else
+    set -l parent_dir (dirname "$PWD")
+    set_color blue
+    if [ "$parent_dir" = "$HOME" ]
+      echo -n '~'
+    else
+      echo -n (basename "$parent_dir")
+    end
+    echo -n ' → '
+    set_color green; echo -n (basename "$PWD")
+  end
+  echo -n ' '
 
   # show some pretty git information if we're in a git repo
-  set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
-  set -l git_prompt (
-    if [ "$git_branch" ]
-      # color the branch name differently if the working tree is dirty
-      set -l git_color $yellow
-      set -l git_status (git status --porcelain)
-      set -l git_color (
-        if [ "$git_status" ]; echo $lred; else; echo $yellow; end
-      )
-      # check how far ahead/behind the branch is
-      set -l git_upstream_status (
-        git rev-parse '@{u}' >/dev/null ^&1
-        if [ $status -eq 0 ]
-          # if an upstream is set, display information
-          set -l git_commits_ahead (git rev-list '@{u}..HEAD' | wc -l | awk \{'print $1'\})
-          set -l git_commits_behind (git rev-list 'HEAD..@{u}' | wc -l | awk \{'print $1'\})
-          if [ \( $git_commits_ahead -eq 0 \) -a \( $git_commits_behind -eq 0 \) ]
-            echo -n "$blue⦿"
-          end
-          if [ $git_commits_behind -gt 0 ]
-            echo -n "$red↓$git_commits_behind"
-          end
-          if [ $git_commits_ahead -gt 0 ]
-            echo -n "$lred↑$git_commits_ahead"
-          end
-        else
-          # otherwise, indicate that an upstream is unknown
-          echo "$lred$question_mark_in_circle"
+  set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
+  if [ -n "$git_branch" ]
+    if git rev-parse '@{u}' >/dev/null 2>&1
+      # if an upstream is set, check how far ahead/behind the branch is
+      set -l git_commits_ahead (git rev-list '@{u}..HEAD' | wc -l | awk \{'print $1'\})
+      set -l git_commits_behind (git rev-list 'HEAD..@{u}' | wc -l | awk \{'print $1'\})
+      if [ \( "$git_commits_ahead" -eq 0 \) -a \( "$git_commits_behind" -eq 0 \) ]
+        set_color blue; echo -n '⦿'
+      else
+        if [ "$git_commits_behind" -gt 0 ]
+          set_color red; echo -n "↓$git_commits_behind"
         end
-      )
-      echo -s "$git_upstream_status $blue" '[' "$git_color$git_branch$reset$blue] "
+        if [ "$git_commits_ahead" -gt 0 ]
+          set_color brred; echo -n "↑$git_commits_ahead"
+        end
+      end
+    else
+      # otherwise, indicate that an upstream is unknown
+      set_color brred; echo -n "$question_mark_in_circle"
     end
-  )
 
-  echo -s "$red# $location$blue $git_prompt$blue$lcyan" "λ:$reset "
+    set_color blue; echo -n ' ['
+    # color the branch name differently if the working tree is dirty
+    if [ (count (git status --porcelain)) -gt 0 ]
+      set_color brred
+    else
+      set_color yellow
+    end
+    echo -n "$git_branch"
+    set_color blue; echo -n '] '
+  end
+
+  if [ "$command_status" -eq 0 ]
+    set_color brcyan; echo -n 'λ: '
+  else
+    set_color red; echo -n 'λ! '
+  end
+  set_color normal
 end
