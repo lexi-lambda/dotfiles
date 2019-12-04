73f328bf914910b1eff63f0dde51359bf9c8ecb3
Alexis King <lexi.lambda@gmail.com>
2019-12-03 18:52:25 -0600

    postgresql: Create postgis_raster in template1

--- postgresql/initialize_template1.sh
+++ postgresql/initialize_template1.sh
@@ -2,21 +2,24 @@
 set -euxo pipefail
 
 create_extension_in_separate_schema () {
-  local extension_name="$1"
+  local schema_name="$1"
   psql -a -v ON_ERROR_STOP=on -1 -d template2 <<SQL
-    CREATE SCHEMA $extension_name;
-    GRANT USAGE ON SCHEMA $extension_name TO PUBLIC;
-    GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA $extension_name TO PUBLIC;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA $extension_name GRANT EXECUTE ON FUNCTIONS TO PUBLIC;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA $extension_name GRANT USAGE ON TYPES TO PUBLIC;
-    CREATE EXTENSION IF NOT EXISTS $extension_name WITH SCHEMA $extension_name;
+    CREATE SCHEMA $schema_name;
+    GRANT USAGE ON SCHEMA $schema_name TO PUBLIC;
+    GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA $schema_name TO PUBLIC;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA $schema_name GRANT EXECUTE ON FUNCTIONS TO PUBLIC;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA $schema_name GRANT USAGE ON TYPES TO PUBLIC;
 SQL
+  for extension_name in "$@"; do
+    psql -a -v ON_ERROR_STOP=on -1 -d template2 -c \
+      "CREATE EXTENSION IF NOT EXISTS $extension_name WITH SCHEMA $schema_name;"
+  done
 }
 
 psql -d postgres -c 'CREATE DATABASE template2 TEMPLATE template0'
 
 create_extension_in_separate_schema pgcrypto
-create_extension_in_separate_schema postgis
+create_extension_in_separate_schema postgis postgis_raster
 
 psql -a -v ON_ERROR_STOP=on -d postgres <<SQL
   ALTER DATABASE template1 IS_TEMPLATE false;
