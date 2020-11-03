da2671b2a1c15c7e8e7a69a46ea83a41d9ae4d45
Alexis King <lexi.lambda@gmail.com>
2020-11-03 12:33:59 -0600

    fish/is-wsl: Guard against non-existence of /proc/version

--- fish/functions/is-wsl.fish
+++ fish/functions/is-wsl.fish
@@ -1,3 +1,3 @@
 function is-wsl -d 'Tests if fish is running under Windows Subsystem for Linux'
-  string match -qie microsoft </proc/version
+  test -f /proc/version; and string match -qie microsoft </proc/version
 end
